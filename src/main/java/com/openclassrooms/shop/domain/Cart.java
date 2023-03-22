8eea9a172223a4a018da79ac0910bbafbf064ab5
Isiah <idaniels051@gmail.com>
2023-03-22 18:57:00 -0500

    5/6 tests passing

--- src/main/java/com/openclassrooms/shop/domain/Cart.java
+++ src/main/java/com/openclassrooms/shop/domain/Cart.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class Cart {
     //Private fields to be added
     List<CartLine> cartLines = new ArrayList<>();
+    Double total = 0.0;
 
     /**
      * @return the actual cartline list
      */
     public List<CartLine> getCartLineList() {
-        //TODO implement the method
         return cartLines;
     }
 
@@ -22,8 +22,6 @@ public class Cart {
      * @param quantity the quantity
      */
     public void addItem(Product product, int quantity) {
-        // TODO implement the method
-        //Add if-check with incrementation if already exists
         for (CartLine cartLine : cartLines) {
             if (cartLine.getProduct().equals(product)) {
                 int newQuantity = cartLine.getQuantity() + quantity;
@@ -50,7 +48,10 @@ public class Cart {
      */
     public double getTotalValue() {
         //TODO implement the method
-        return 0.0;
+        for (CartLine cartline: cartLines) {
+           total += cartline.getSubtotal();
+        }
+        return total;
 
     }
 
@@ -59,15 +60,8 @@ public class Cart {
      */
     public double getAverageValue() {
         // TODO implement the method
-        Double runningTotal = 0.0;
-        Double average = 0.0;
-        for (CartLine cartLine : cartLines) {
-            runningTotal +=cartLine.getSubtotal();
-            if(!cartLines.iterator().hasNext()){
-                average = runningTotal / cartLines.size();
-            }
-        }
-        return average;
+        Double averageValue = getTotalValue() / cartLines.size();
+        return averageValue;
     }
 
     /**
@@ -75,7 +69,6 @@ public class Cart {
      * @return getProductById in the cart if it finds it
      */
     public Product findProductInCartLines(Long productId) {
-        // TODO implement the method
         for (int i = 0; i < cartLines.size(); i++){
             if (cartLines.get(i).getProduct().getId().equals(productId)){
                 return cartLines.get(i).getProduct();
